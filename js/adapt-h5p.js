d5a53cc4596ebe31efdc9ad049035e8200c237f0
Simon  Date <simondate@yahoo.co.uk>
2016-11-01 16:41:11 +0000

    Working with new name

---
+++ js/adapt-h5p.js
@@ -0,0 +1,54 @@
+/*
+ * adapt-contrib-responsiveIframe
+ * License - http://github.com/adaptlearning/adapt_framework/LICENSE
+ * Maintainers - Kevin Corry <kevinc@learningpool.com>
+ */
+define(function(require) {
+
+    var ComponentView = require("coreViews/componentView");
+    var Adapt = require("coreJS/adapt");
+
+    var H5p = ComponentView.extend({
+
+        events: {
+            'inview': 'inview'
+        },
+
+        preRender: function() {
+            console.log(this);
+            console.log(top.Frame0);
+            this.listenTo(Adapt, 'device:changed', this.resizeControl);
+        },
+
+        postRender: function() {
+            var that = this;
+            that.setReadyStatus();
+            this.$('.h5p-iframe').ready(function() {
+                that.resizeControl(Adapt.device.screenSize);
+            });
+        },
+
+        inview: function(event, visible) {
+            if (visible) {
+                this.setCompletionStatus();
+            }
+        },
+
+        resizeControl: function(size) {
+            var width = this.$('.h5p-iframe').attr('data-width-' + size);
+            var height = this.$('.h5p-iframe').attr('data-height-' + size);
+            this.$('.h5p-iframe').width(width);
+            this.$('.h5p-iframe').height(height);
+        }
+
+    });
+
+    window.onmessage = function(e) {
+        if (e.data == 'hello') {
+            alert('It works!');
+        }
+    };
+
+    Adapt.register("h5p", H5p);
+
+});
